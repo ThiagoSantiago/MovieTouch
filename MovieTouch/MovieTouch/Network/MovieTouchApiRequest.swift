b36fd77423dffd09442e5e3f49ff15703ec3af08
Thiago Santiago <thiagoalexandres@gmail.com>
2019-03-01 10:58:16 -0300

    Create the network structure.

---
+++ MovieTouch/MovieTouch/Network/MovieTouchApiRequest.swift
@@ -0,0 +1,68 @@
+//
+//  MovieTouchApiRequest.swift
+//  MovieTouch
+//
+//  Created by Thiago Santiago on 3/1/19.
+//  Copyright © 2019 Thiago Santiago. All rights reserved.
+//
+
+import Foundation
+
+enum Result<T> {
+    case success(T)
+    case failure(MovieTouchApiError)
+}
+
+protocol MovieTouchApiRequestProtocol {
+    func request(_ request: MovieTouchApiSetupProtocol, completion: @escaping (Result<Data>) -> Void)
+}
+
+
+class GitAllStarsApiRequest: MovieTouchApiRequestProtocol {
+    
+    func request(_ request: MovieTouchApiSetupProtocol, completion: @escaping (Result<Data>) -> Void) {
+        
+        guard let urlRequest = URL(string: request.endpoint) else {
+            completion(.failure(.badUrl))
+            return
+        }
+        
+        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
+            
+            if let error = error {
+                completion(.failure(.unknown(error.localizedDescription)))
+            }
+            
+            guard let data = data else {
+                completion(.failure(.brokenData))
+                return
+            }
+            
+            guard let httpResponse = response as? HTTPURLResponse else {
+                completion(.failure(.unknown("Could not cast to HTTPURLResponse object.")))
+                return
+            }
+            
+            completion(self.handler(statusCode: httpResponse.statusCode, dataResponse: data))
+            
+            }.resume()
+    }
+    
+    private func handler(statusCode: Int, dataResponse: Data) -> Result<Data> {
+        
+        switch statusCode {
+        case 200...299:
+            return Result.success(dataResponse)
+        case 403:
+            return Result.failure(.authenticationRequired)
+            
+        case 404:
+            return Result.failure(.couldNotFindHost)
+            
+        case 500:
+            return Result.failure(.badRequest)
+            
+        default: return Result.failure(.unknown(""))
+        }
+    }
+}
---
+++ MovieTouch/MovieTouch/Network/UpcomingMoviesServiceSetup.swift
@@ -0,0 +1,25 @@
+//
+//  UpcomingMoviesServiceSetup.swift
+//  MovieTouch
+//
+//  Created by Thiago Santiago on 3/1/19.
+//  Copyright © 2019 Thiago Santiago. All rights reserved.
+//
+
+import Foundation
+
+
+enum UpcomingMoviesServiceSetup: MovieTouchApiSetupProtocol {
+    
+    case fetchUpcomingMovies(page: String)
+    
+    var endpoint: String {
+        switch self {
+            
+        case .fetchUpcomingMovies(let page):
+            let url = Constants.baseUrl + "/movie/upcoming?api_key=\(Constants.apiKey)&language=pt-BR&page=\(page)"
+            
+            return url
+        }
+    }
+}
