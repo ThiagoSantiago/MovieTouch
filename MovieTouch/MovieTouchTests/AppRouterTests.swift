7e4f2509b68ff67e33c3a7a6f71df67da755b85e
Thiago Santiago <thiagoalexandres@gmail.com>
2019-03-03 00:26:51 -0300

    Add tests for the router.

---
+++ MovieTouch/MovieTouchTests/AppRouterTests.swift
@@ -0,0 +1,30 @@
+//
+//  AppRouterTests.swift
+//  MovieTouchTests
+//
+//  Created by Thiago Santiago on 3/3/19.
+//  Copyright © 2019 Thiago Santiago. All rights reserved.
+//
+
+import XCTest
+@testable import MovieTouch
+
+class AppRoutertests: XCTestCase {
+    var router: AppRouter!
+    var navigationController: UINavigationController!
+    
+    override func setUp() {
+        navigationController = UINavigationController()
+        router = AppRouter()
+        router.navigation = navigationController
+    }
+    
+    func testWhenCallRouteToHomeThenNavigationPushReviewsViewController() {
+        router.routeToHome()
+        
+        XCTAssertEqual(router.navigation, navigationController)
+        XCTAssertTrue(
+            router.navigation.viewControllers.first!.isKind(of: UpcomingMoviesViewController.self)
+        )
+    }
+}
