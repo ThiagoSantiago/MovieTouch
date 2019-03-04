243f9436ba351db0ef69ed317c68deae4bc7029e
Thiago Santiago <thiagoalexandres@gmail.com>
2019-03-03 21:16:16 -0300

    Update the router tests.

--- MovieTouch/MovieTouchTests/AppRouterTests.swift
+++ MovieTouch/MovieTouchTests/AppRouterTests.swift
@@ -19,8 +19,34 @@ class AppRoutertests: XCTestCase {
         router.navigation = navigationController
     }
     
-    func testWhenCallRouteToHomeThenNavigationPushReviewsViewController() {
+    func testWhenCallRouteToUpcomingMoviesThenNavigationPushUpcomingMoviesViewController() {
         router.routeToUpcomingMovies()
+        
+        XCTAssertEqual(router.navigation, navigationController)
+        XCTAssertTrue(
+            router.navigation.viewControllers.first!.isKind(of: UpcomingMoviesViewController.self)
+        )
+    }
+    
+    func testWhenCallRouteToMovieDetailsThenNavigationPushMovieDetailsViewController() {
+        
+        let viewModel = UpcomingMoviesViewModel(genres: " Animation | Family | Adventure |", poster: "/xvx4Yhf0DVH8G4LzNISpMfFBDy2.jpg", backdrop: "/h3KN24PrOheHVYs9ypuOIdFBEpX.jpg", overview: "As Hiccup fulfills his dream of creating a peaceful dragon utopia, Toothless’ discovery of an untamed, elusive mate draws the Night Fury away. When danger mounts at home and Hiccup’s reign as village chief is tested, both dragon and rider must make impossible decisions to save their kind.", movieName: "How to Train Your Dragon: The Hidden World", releaseDate: "03/01/2019")
+        
+        router.routeToMovieDetails(viewModel: viewModel)
+        
+        XCTAssertEqual(router.navigation, navigationController)
+        XCTAssertTrue(
+            router.navigation.viewControllers.first!.isKind(of: MovieDetailsViewController.self)
+        )
+    }
+    
+    func testWhenCallPopViewControllerThenNavigationPopTheTopViewController() {
+        router.routeToUpcomingMovies()
+        
+        let viewModel = UpcomingMoviesViewModel(genres: " Animation | Family | Adventure |", poster: "/xvx4Yhf0DVH8G4LzNISpMfFBDy2.jpg", backdrop: "/h3KN24PrOheHVYs9ypuOIdFBEpX.jpg", overview: "As Hiccup fulfills his dream of creating a peaceful dragon utopia, Toothless’ discovery of an untamed, elusive mate draws the Night Fury away. When danger mounts at home and Hiccup’s reign as village chief is tested, both dragon and rider must make impossible decisions to save their kind.", movieName: "How to Train Your Dragon: The Hidden World", releaseDate: "03/01/2019")
+        
+        router.routeToMovieDetails(viewModel: viewModel)
+        router.popViewController()
         
         XCTAssertEqual(router.navigation, navigationController)
         XCTAssertTrue(
